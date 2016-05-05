76f5583c8fba1fb6ab245aaeb8f8e7fabcb3fc90
Tobin Yehle <tobinyehle@gmail.com>
2016-05-05 09:53:13 -0600

    recentering done

--- Main.elm
+++ Main.elm
@@ -2,6 +2,7 @@ import Color exposing (..)
 import Graphics.Collage exposing (..)
 import Graphics.Element exposing (..)
 import Time exposing (..)
+import Random exposing (Generator, Seed, generate)
 
 type alias State = { x : Float
                    , y : Float
@@ -25,7 +26,7 @@ height : Float
 height = 800
 
 particleMass : Float
-particleMass = 0.001
+particleMass = 10
 
 attractorMass : Float
 attractorMass = 10
@@ -40,16 +41,38 @@ attractorY = 0
 main : Signal Element
 main =
   let
-    start = [ { x = -200, y = 0, vx = 0.02, vy = -0.02, size = 20, color = darkPurple }
-            , { x = 200, y = 0,  vx = 0.02, vy = -0.04, size = 5, color = charcoal }
-            , { x = 200, y = 20,  vx = 0.02, vy = -0.04, size = 5, color = charcoal }
-            , { x = 200, y = 40,  vx = 0.02, vy = -0.04, size = 5, color = charcoal }
-            , { x = 200, y = 60,  vx = 0.02, vy = -0.04, size = 5, color = charcoal }
-            , { x = 200, y = 80,  vx = 0.02, vy = -0.04, size = 5, color = charcoal }
+  --{ x = -200, y = 0, vx = 0.02, vy = -0.02, size = 20, color = darkPurple }
+    start = [ { x = 200, y = 0,  vx = 0.02, vy = 0.04, size = 5, color = charcoal }
+            , { x = 200, y = 20, vx = -0.02, vy = 0.04, size = 5, color = charcoal }
+            , { x = 200, y = 40, vx = 0.02, vy = 0.04, size = 5, color = charcoal }
+            , { x = 200, y = 60, vx = -0.02, vy = 0.04, size = 5, color = charcoal }
+            , { x = 200, y = 80, vx = 0.02, vy = 0.04, size = 5, color = charcoal }
+            , { x = -100, y = 0,  vx = -0.02, vy = -0.04, size = 5, color = charcoal }
+            , { x = -100, y = 20, vx = 0.02, vy = -0.04, size = 5, color = charcoal }
+            , { x = -50, y = 40, vx = -0.02, vy = -0.04, size = 5, color = charcoal }
+            , { x = -100, y = 60, vx = 0.02, vy = -0.04, size = 5, color = charcoal }
+            , { x = -100, y = 80, vx = -0.02, vy = -0.04, size = 5, color = charcoal }
             ]
     accumulated = Signal.foldp gravitateAll start (fps 60)
   in
     Signal.map draw accumulated
+
+
+genParticle : Random.Seed -> (State, Random.Seed)
+genParticle s0 =
+  let
+    v = Random.float -0.05 0.05
+    (vx, s1) = generate v s0
+    (vy, s2) = generate v s1
+
+    pos = Random.float -200 200
+    (x, s3) = generate pos s2
+    (y, s4) = generate pos s3
+
+    p = { x = x, y = y, vx = vx, vy = vy, size = 5, color = charcoal }
+  in
+    (p, s4)
+
 
 
 ball : State -> Form
@@ -64,12 +87,35 @@ draw states =
     (filled darkGrey (rect width height) :: (filled grey (rect 5 5)) :: List.map ball states)
 
 
+
 gravitateAll : Time -> List State -> List State
-gravitateAll t states = List.map (gravitate t (attractorX, attractorY) attractorMass) states
+gravitateAll t states =
+  --List.map (gravitate t (attractorX, attractorY) attractorMass) states
+  let
+    center = findCenter states
+    centered = List.map (recenter center) states
+    sorted = List.sortBy (distance center) centered
+  in
+    nbody t center sorted
+
+recenter : Position -> State -> State
+recenter (dx, dy) s = { s | x = s.x - dx, y = s.y - dy }
+
+distance : Position -> State -> Float
+distance (x, y) s = sqrt ((x - s.x)*(x - s.x) + (y - s.y)*(y - s.y))
+
+findCenter : List State -> Position
+findCenter states =
+  let
+    n = toFloat ( List.length states )
+    x = List.sum ( List.map .x states ) / n
+    y = List.sum ( List.map .y states ) / n
+  in
+    (x, y)
 
 -- The list of particles should be sorted by distance from the center
-nbody : Time -> List State -> (Float, Float) -> List State
-nbody t particles center =
+nbody : Time -> Position -> List State -> List State
+nbody t center particles =
   let
     updateParticle s n = gravitate t center ((toFloat n)*particleMass) s
   in
