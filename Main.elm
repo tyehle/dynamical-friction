22bd13a79497234dfc256770e2dc3877abbd26d0
Tobin Yehle <tobinyehle@gmail.com>
2016-06-29 10:17:39 -0600

    program compiles, but everything moves slower than before

--- Main.elm
+++ Main.elm
@@ -2,21 +2,23 @@ import Color exposing (..)
 import Collage exposing (..)
 import Element exposing (..)
 import Time exposing (..)
-import Random exposing (Generator, Seed, generate)
+import Random exposing (Generator, Seed, step)
 import Window exposing (width, height)
 import AnimationFrame
 import Html.App as App
-import Html
+import Html exposing (..)
 import Task
 
 type alias World = { attractor : Particle
+                   , attractorMass : Mass
                    , particles : List Particle
+                   , particleMass : Mass
+                   , windowSize : Window.Size
                    }
 
 type alias Particle = { pos : Vec2
                       , velocity : Vec2
                       , size : Float
-                      , mass : Mass
                       , color : Color
                       }
 
@@ -30,6 +32,8 @@ type Message = Step Time | Resize Window.Size
 g : Float
 g = 0.00002
 
+defaultSize : Int
+defaultSize = 200
 
 
 -- INIT --
@@ -37,24 +41,24 @@ g = 0.00002
 init : (World, Cmd Message)
 init = 
   let
-    mass  = { pos = {x=-200, y=0}, velocity = {x=0.02, y=0}, mass = 2, size = 20, color = lightPurple }
+    mass  = { pos = {x=-200, y=0}, velocity = {x=0.02, y=0}, size = 20, color = lightPurple }
     particles = genParticles 50 (Random.initialSeed 2)
-    initialResize = Task.perform (\_ -> Resize {x=800, y=800}) Resize Window.size
+    initialResize = Task.perform (\_ -> Resize {height=defaultSize, width=defaultSize}) Resize Window.size
   in
-    ({attractor = mass, particles = particles}, Cmd.none)
+    ({attractor = mass, attractorMass = 2, particles = particles, particleMass = 0.5, windowSize = {height=defaultSize, width=defaultSize}}, initialResize)
 
 genParticles : Int -> Seed -> List Particle
 genParticles = genParticlesAccumulated []
 
 genParticlesAccumulated : List Particle -> Int -> Seed -> List Particle
-genParticlesAccumulated n s ps =
+genParticlesAccumulated ps n s =
   if n <= 0 then
     ps
   else
     let
       (p, s') = genParticle s
     in
-      genParticles (n-1) s' (p :: ps)
+      genParticlesAccumulated (p :: ps) (n-1) s'
 
 genParticle : Seed -> (Particle, Seed)
 genParticle s0 =
@@ -62,25 +66,146 @@ genParticle s0 =
     theta = Random.float 0 (2*pi)
 
     v = Random.float -0.05 0.05
-    (vr, s1) = generate v s0
-    (vt, s2) = generate theta s1
+    (vr, s1) = step v s0
+    (vt, s2) = step theta s1
     vx = vr * cos vt
     vy = vr * sin vt
 
     pos = Random.float -200 200
-    (r, s3) = generate pos s2
-    (t, s4) = generate theta s3
+    (r, s3) = step pos s2
+    (t, s4) = step theta s3
     x = r * cos t
     y = r * sin t
 
-    p = { pos = {x=x, y=y}, velocity = {x=vx, y=vy}, mass = 0.5, size = 3, color = grey }
+    p = { pos = {x=x, y=y}, velocity = {x=vx, y=vy}, size = 3.0, color = grey }
   in
     (p, s4)
 
 
 
---main : Signal Element
---main = Signal.map2 draw Window.dimensions simulate
+-- VIEW --
+
+view : World -> Html Message
+view world = toHtml (drawWorld world)
+
+drawBall : Particle -> Form
+drawBall s =
+  filled s.color (circle s.size)
+    |> move (s.pos.x, s.pos.y)
+
+
+drawWorld : World -> Element
+drawWorld {attractor, particles, windowSize} =
+  let
+    {width, height} = windowSize
+    background = filled charcoal (rect (toFloat width) (toFloat height))
+    centerMark = filled lightBlue (rect 5 5)
+    particleCircles = List.map drawBall (attractor :: particles)
+  in
+    collage width height
+      (background :: centerMark :: particleCircles)
+
+
+
+-- UPDATE --
+
+update : Message -> World -> (World, Cmd Message)
+update msg world = case msg of
+  Step t -> (gravitateAll t world, Cmd.none)
+  Resize size -> ({world | windowSize = size}, Cmd.none)
+
+gravitateAll : Time -> World -> World
+gravitateAll t world =
+  let
+    recenter {x,y} s = { s | pos = { x = s.pos.x - x, y = s.pos.y - y } }
+    distance {x,y} s = sqrt ((x - s.pos.x)*(x - s.pos.x) + (y - s.pos.y)*(y - s.pos.y))
+
+    {particles, attractor} = world
+
+    center = findCenter particles
+    centered = List.map (recenter center) particles
+    sorted = List.sortBy (distance center) centered
+  in
+    nbody t center {world | particles = sorted}
+
+findCenter : List Particle -> Vec2
+findCenter particles =
+  let
+    n = toFloat ( List.length particles )
+    x = List.sum ( List.map (\p -> p.pos.x) particles ) / n
+    y = List.sum ( List.map (\p -> p.pos.y) particles ) / n
+  in
+    {x=x, y=y}
+
+-- The list of particles should be sorted by distance from the center
+nbody : Time -> Vec2 -> World -> World
+nbody t center world =
+  let
+    {attractor, attractorMass, particles, particleMass} = world
+    newAttractor = updateAttractor particleMass t particles attractor
+    newParticles = List.indexedMap (updateParticle t attractorMass particleMass center attractor) particles
+  in
+    {world | attractor = newAttractor, particles = newParticles}
+
+
+updateAttractor : Time -> Mass -> List Particle -> Particle -> Particle
+updateAttractor t particleMass particles attractor =
+  let
+    accelForParticle p = acceleration t p.pos particleMass attractor
+    totalAccel = List.foldl (\p a -> vecAdd a (accelForParticle p)) {x=0, y=0} particles
+  in
+    accelerate t totalAccel attractor
+
+
+updateParticle : Time -> Mass -> Mass -> Vec2 -> Particle -> Int -> Particle -> Particle
+updateParticle t attractorMass particleMass center attractor n s =
+  let
+    pa = acceleration t center ((toFloat n)*particleMass) s
+    ma = acceleration t attractor.pos attractorMass s
+  in
+    accelerate t (vecAdd pa ma) s
+
+
+vecAdd : Vec2 -> Vec2 -> Vec2
+vecAdd a b = {x=a.x+b.x, y=a.y+b.y}
+
+
+acceleration : Time -> Vec2 -> Mass -> Particle -> Vec2
+acceleration t {x, y} attractorMass s =
+  let
+    dx  = s.pos.x - x
+    dy  = s.pos.y - y
+    rSq = (abs dx) + (abs dy)
+    a   = -g * attractorMass / rSq
+    r   = sqrt rSq
+    ax  = a * dx / r
+    ay  = a * dy / r
+  in
+    {x=ax, y=ay}
+
+accelerate : Time -> Vec2 -> Particle -> Particle
+accelerate t a s =
+  let
+    vx' = s.velocity.x + a.x*t
+    x'  = s.pos.x + (s.velocity.x + vx')/2*t
+
+    vy' = s.velocity.y + a.y*t
+    y'  = s.pos.y + (s.velocity.y+vy')/2*t
+  in
+    { s | pos = {x=x', y=y'}
+        , velocity = {x=vx', y=vy'}
+    }
+
+
+
+-- SUBSCRIPTIONS --
+
+subscriptions : World -> Sub Message
+subscriptions _ = Sub.batch [AnimationFrame.diffs Step, Window.resizes Resize ]
+
+
+
+-- MAIN --
 
 main : Program Never
 main = App.program { init = init
@@ -88,124 +213,3 @@ main = App.program { init = init
                    , view = view
                    , subscriptions = subscriptions
                    }
-
-
-ball : Particle -> Form
-ball s =
-  filled s.color (circle s.size)
-    |> move (s.x, s.y)
-
-
-draw : (Int, Int) -> World -> Element
-draw (width, height) (mass, particles) =
-  let
-    background = filled charcoal (rect (toFloat width) (toFloat height))
-    centerMark = filled lightBlue (rect 5 5)
-    particleCircles = List.map ball (mass :: particles)
-  in
-    collage width height
-      (background :: centerMark :: particleCircles)
-
-
-
-simulate : Signal World
-simulate =
-  let
-    mass  = { pos = {x=-200, y=0}, velocity = {x=0.02, y=0}, mass = 2, size = 20, color = lightPurple }
-    particles = genParticles 50 (Random.initialSeed 2)
-  in
-    Signal.foldp gravitateAll { attractor = mass, particles = particles } (fps 60)
-
-
-
-
-
-gravitateAll : Time -> World -> World
-gravitateAll t (mass, particles) =
-  --List.map (gravitate t (attractorX, attractorY) attractorMass) particles
-  let
-    recenter (dx, dy) s = { s | x = s.x - dx, y = s.y - dy }
-    distance (x, y) s = sqrt ((x - s.x)*(x - s.x) + (y - s.y)*(y - s.y))
-
-    center = findCenter particles
-    centered = List.map (recenter center) particles
-    sorted = List.sortBy (distance center) centered
-  in
-    nbody t center (mass, sorted)
-
-findCenter : List Particle -> Position
-findCenter particles =
-  let
-    n = toFloat ( List.length particles )
-    x = List.sum ( List.map .x particles ) / n
-    y = List.sum ( List.map .y particles ) / n
-  in
-    (x, y)
-
--- The list of particles should be sorted by distance from the center
-nbody : Time -> Position -> World -> World
-nbody t center (mass, particles) =
-  let
-    newMass = updateMass t particles mass
-    newParticles = mapWithIndex (updateParticle t center mass) particles
-  in
-    (newMass, newParticles)
-
-
-updateMass : Time -> List Particle -> Particle -> Particle
-updateMass t particles attractor =
-  let
-    accelForParticle p = acceleration t (p.x, p.y) particleMass attractor
-    totalAccel = List.foldl (\p a -> positionAdd a (accelForParticle p)) (0, 0) particles
-  in
-    accelerate t totalAccel attractor
-
-
-updateParticle : Time -> Position -> Particle -> Int -> Particle -> Particle
-updateParticle t center mass n s =
-  let
-    pa = acceleration t center ((toFloat n)*particleMass) s
-    ma = acceleration t (mass.x, mass.y) attractorMass s
-  in
-    accelerate t (positionAdd pa ma) s
-
-
-positionAdd : Position -> Position -> Position
-positionAdd (ax, ay) (bx, by) = (ax+bx, ay+by)
-
-mapWithIndex : (Int -> a -> b) -> List a -> List b
-mapWithIndex fn items =
-  let
-    op item (index, res) = (index + 1, fn index item :: res)
-    folded = List.foldl op (0, []) items
-  in
-    snd folded
-
-
-acceleration : Time -> Position -> Mass -> Particle -> Position
-acceleration t (x, y) m s =
-  let
-    dx  = s.x - x
-    dy  = s.y - y
-    rSq = (abs dx) + (abs dy)
-    a   = -g * attractorMass / rSq
-    r   = sqrt rSq
-    ax  = a * dx / r
-    ay  = a * dy / r
-  in
-    (ax, ay)
-
-accelerate : Time -> Position -> Particle -> Particle
-accelerate t (ax, ay) s =
-  let
-    vx' = s.vx + ax*t
-    x'  = s.x + (s.vx + vx')/2*t
-
-    vy' = s.vy + ay*t
-    y'  = s.y + (s.vy+vy')/2*t
-  in
-    { s | x = x'
-        , y = y'
-        , vx = vx'
-        , vy = vy'
-    }
